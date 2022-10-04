01876ddcc5038e1fbdeda766f072fe359886e67d
yadev.jayachandran <yadev@krews.com>
2022-10-05 03:22:21 +0400

    Added mutation to save post

--- src/store/posts/mutations.js
+++ src/store/posts/mutations.js
@@ -1,3 +1,18 @@
 export function updateModalStatus(state, value) {
   state.modal_status = value
 }
+
+export function storePostData(state, post) {
+  if (post) {
+
+    let postTempObject = {}
+    postTempObject.content = post.content ? post.content : ''
+    postTempObject.image = post.image ? post.image : null
+    postTempObject.is_public = post.is_public ? post.is_public : false
+
+    postTempObject.likes = 0
+    postTempObject.id = state.posts.length
+
+    state.posts.push(postTempObject)
+  }
+}
