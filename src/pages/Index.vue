fac4336de542e9f6d20ec3cccaa5ac7c8b7c006f
yadev.jayachandran <yadev@krews.com>
2022-10-04 22:27:49 +0400

    Updated Index page: - Background update - Added card area for displaying posts

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,17 +1,37 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    />
+  <q-page class="page-bg">
+    <div class="row">
+      <div class="col-12">
+        <div style="width: 100%">
+          <q-card class="main-area q-pa-md">
+            <newPostActionComponent></newPostActionComponent>
+          </q-card>
+        </div>
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import newPostActionComponent from "src/components/Post/NewPostActionComponent.vue";
 
 export default defineComponent({
   name: "PageIndex",
+  components: { newPostActionComponent },
 });
 </script>
+
+<style scoped>
+.page-bg {
+  background: rgb(198, 227, 255);
+}
+.main-area {
+  background: white;
+  border-radius: 10px;
+  max-width: 800px;
+  width: 100%;
+  margin: auto;
+  margin-top: 50px;
+}
+</style>
