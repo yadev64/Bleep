ee6912077f23c499834fa61e44ba050b9f167e7a
yadev.jayachandran <yadev@krews.com>
2022-10-04 22:26:54 +0400

    NavBar update - Logo position update

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,15 +2,17 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="bg-white toolbar">
+        <q-space></q-space>
+
         <q-img
-          class="q-ml-xl q-mt-md"
+          class="q-mt-md"
           src="~assets/bleep_full.png"
           fit="contain"
-          style="height: 100px; width: 130px"
+          style="height: 100px; width: 110px"
         ></q-img>
         <!-- <q-toolbar-title class="text-blue"> Bleep </q-toolbar-title> -->
         <q-space></q-space>
-        <div class="text-blue">Hi Steve</div>
+        <!-- <div class="text-blue">Hi Steve</div> -->
       </q-toolbar>
     </q-header>
 
