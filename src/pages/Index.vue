e49777f50bdcc6e7514f7f0b3291432748c54e9c
yadev.jayachandran <yadev@krews.com>
2022-10-05 01:44:57 +0400

    Linked NewPostModal component

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -4,21 +4,27 @@
       <div class="col-12">
         <div style="width: 100%">
           <q-card class="main-area q-pa-md">
+            <h6 class="q-ma-xs text-accent">Home</h6>
             <newPostActionComponent></newPostActionComponent>
           </q-card>
         </div>
       </div>
     </div>
   </q-page>
+  <createPostModalComponent></createPostModalComponent>
 </template>
 
 <script>
 import { defineComponent } from "vue";
 import newPostActionComponent from "src/components/Post/NewPostActionComponent.vue";
+import createPostModalComponent from "src/components/Post/CreatePostModalComponent.vue";
 
 export default defineComponent({
   name: "PageIndex",
-  components: { newPostActionComponent },
+  components: {
+    newPostActionComponent,
+    createPostModalComponent,
+  },
 });
 </script>
 
@@ -29,7 +35,7 @@ export default defineComponent({
 .main-area {
   background: white;
   border-radius: 10px;
-  max-width: 800px;
+  max-width: 600px;
   width: 100%;
   margin: auto;
   margin-top: 50px;
